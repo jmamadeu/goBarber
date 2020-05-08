15b9272194bd7623d74b210a19ff049d2d8f38a6
jmamadeu <jmamadeu2000@gmail.com>
2020-05-08 06:53:27 +0100

    :zap: feat: Update app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+import connection from './database';
+import routes from './routes/index.routes';
+
+connection();
+
 app.use(express.json());
+app.use(routes);
 
 export default app;
